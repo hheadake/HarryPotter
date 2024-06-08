bcf9720b4921578025a4b6d95e9c11de142f4bb2
hheadake <113627073+hheadake@users.noreply.github.com>
2024-06-08 16:49:20 +0300

    Update index.js

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ app.set('view engine', '.hbs');
 app.set('views', 'src/views');
 
 
-
+app.use(express.static(path.join(__dirname, '/src/views/layouts/main.hbs')))
 app.use(express.static(path.resolve(__dirname, 'public')));
 app.use(express.urlencoded({ extended:false }))
 app.use(cookieParser());
